adce372341f29acd05180370a1a49902c175c872
elsaamelia15 <93023055+elsaamelia15@users.noreply.github.com>
2022-02-26 16:05:56 +0700

    Add files via upload

---
+++ Praktikum_1/pertemuan_1.php
@@ -0,0 +1,23 @@
+<?php
+  // Variable user
+  $nama = 'Elsa';
+  $umur = 19;
+  $berat = 52;
+
+  echo 'Nama saya '.$nama.' umur saya '.$umur.' berat saya '.$berat;
+  echo "<br />";
+  echo "<h1 style='color: blue'>Nama saya $nama, umur saya $umur, berat saya $berat</h1>";
+  echo "<br />";
+
+  // Variable sistem
+  // variable built in php
+  echo 'Dokumen Root '.$_SERVER["DOCUMENT_ROOT"];
+  echo "<br />";
+
+  // Variable konstan
+  // adalah variable yang tidak bisa diubah nilainya
+  define('makanan', 'sushi');
+  // define('makanan', 'jambu');
+  echo '<h1 style="color: blue">'.makanan.'</h1>';
+  echo makanan;
+?>
